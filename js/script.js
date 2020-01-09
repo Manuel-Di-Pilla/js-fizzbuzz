3d2d390228e409d963921fce0f9cc4c9422a3a93
Manuel <manueldipilla3@gmail.com>
2020-01-09 19:30:05 +0100

    esercizio completo

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var text;
+var fizz;
+var buzz;
+var fizzbuzz;
+
+for (var i = 1; i < 101 ; i++) {
+  fizz = i % 3;
+  buzz = i % 5;
+  fizzbuzz = i % 15;
+
+  if (fizz == 0) {
+    text = 'fizz';
+    console.log(text);
+  }
+  else if (buzz == 0) {
+    text = 'buzz';
+    console.log(text);
+  }
+  else if (fizzbuzz == 0) {
+    text = 'fizzbuzz';
+    console.log(text);
+  }
+  else {
+    text = i;
+  }
+  document.getElementById('num').innerHTML += '<li>' + text + '</li>';
+}
