5b0faa70af224a6a017f71b8f8c0ac564d5f0afd
Manuel <manueldipilla3@gmail.com>
2020-01-09 19:35:05 +0100

    Ciclo con while

--- js/script.js
+++ js/script.js
@@ -3,10 +3,34 @@ var fizz;
 var buzz;
 var fizzbuzz;
 
-for (var i = 1; i < 101 ; i++) {
-  fizz = i % 3;
-  buzz = i % 5;
-  fizzbuzz = i % 15;
+// for (var i = 1; i < 101 ; i++) {
+//   fizz = i % 3;
+//   buzz = i % 5;
+//   fizzbuzz = i % 15;
+//
+//   if (fizz == 0) {
+//     text = 'fizz';
+//     console.log(text);
+//   }
+//   else if (buzz == 0) {
+//     text = 'buzz';
+//     console.log(text);
+//   }
+//   else if (fizzbuzz == 0) {
+//     text = 'fizzbuzz';
+//     console.log(text);
+//   }
+//   else {
+//     text = i;
+//   }
+//   document.getElementById('num').innerHTML += '<li>' + text + '</li>';
+// }
+
+j = 1;
+while (j < 101) {
+  fizz = j % 3;
+  buzz = j % 5;
+  fizzbuzz = j % 15;
 
   if (fizz == 0) {
     text = 'fizz';
@@ -21,7 +45,8 @@ for (var i = 1; i < 101 ; i++) {
     console.log(text);
   }
   else {
-    text = i;
+    text = j;
   }
   document.getElementById('num').innerHTML += '<li>' + text + '</li>';
+  j++
 }
